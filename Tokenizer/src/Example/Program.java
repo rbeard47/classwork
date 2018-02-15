df6c4956eea27df1f22e5baa6e4754eb8d44f2aa
Robert Beard <rbeard47@hotmail.com>
2018-02-14 19:50:49 -0500

    Added comments

--- Tokenizer/src/Example/Program.java
+++ Tokenizer/src/Example/Program.java
@@ -9,7 +9,7 @@ import Example.Tokenizer.Token;
 import java.util.Iterator;
 
 /**
- *
+ * This Program class is the is the main executable and contains the main function
  * @author Rob
  */
 public class Program {
@@ -19,18 +19,25 @@ public class Program {
    */
   public static void main(String[] args) {
     
+    // Create a new tokenizer
     Tokenizer tokenizer = new Tokenizer("Hello &56 World!");
     
+    // Loop through the tokens until you find the EOF token
     for(Token t = tokenizer.Next(); t != Token.EOF; t = tokenizer.Next()){
       System.out.println(t);
     }
     
+    // Create a new Lexer object
     Lexer lexer = new Lexer("This if 32434 \t is for a test 55 ^#$#$435#%%$");
     
+    // Call the Lex() method which loops through the input string and tokenizes it
     lexer.Lex();
     
+    // Get an iterator to use to loop through the tokens
     Iterator<Example.Token> i = lexer.iterator();
     
+    // Use the iterator to loop through each token and dump info about it such as TokenType,
+    // where in the string the token start and stops and the string value that was stored.
     while(i.hasNext()) {
       Example.Token t = i.next();
       System.out.format("type=%s begin=%d beyond=%d value=%s\n", t.getTokenType(),
